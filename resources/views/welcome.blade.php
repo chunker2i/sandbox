9b8500eef92b8aba6f4b2c25d4025ad3fa8ad237
Ivan Artamonov <artamonov.i@studio2i.ru>
2025-02-25 10:42:40 +0300

    init

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Laravel</title>
+        @vite(['resources/scss/app.scss', 'resources/js/app.js'])
+    </head>
+    <body class="body">
+
+    <h1>Test</h1>
+
+    <button type="button" class="button">
+	    Кнопка
+    </button>
+
+    </body>
+</html>
---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import path from 'path';
+
+export default defineConfig({
+    resolve: {
+        alias: {
+            '@config': path.resolve(__dirname, 'resources/scss/config'),
+            '@core': path.resolve(__dirname, 'resources/scss/core')
+        }
+    },
+    plugins: [
+        laravel({
+            input: ['resources/scss/app.scss', 'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
+});
