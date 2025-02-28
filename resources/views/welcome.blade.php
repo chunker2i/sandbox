e3c5cd17e34eee264222ff29d76f0a35f9d6c52b
Ivan Artamonov <artamonov.i@studio2i.ru>
2025-02-28 22:24:25 +0300

    Подключены миксины вьюпортов

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,20 @@
     </head>
     <body class="body">
 
-    <h1>Test</h1>
+    <h1>Тест вьюпорта</h1>
+
+    <div class="viewports">
+	    <p class="viewport viewport_mobile">Мобильное устройство</p>
+	    <p class="viewport viewport_phone">Смартфон</p>
+	    <p class="viewport viewport_phone-portrait">Смартфон вертикальный</p>
+	    <p class="viewport viewport_phone-landscape">Смартфон горизонтальный</p>
+	    <p class="viewport viewport_tablet">Планшет</p>
+	    <p class="viewport viewport_tablet-portrait">Планшет вертикальный</p>
+	    <p class="viewport viewport_tablet-landscape">Планшет горизонтальный</p>
+	    <p class="viewport viewport_desktop">Десктоп</p>
+	    <p class="viewport viewport_laptop">Ноутбук</p>
+	    <p class="viewport viewport_wide">Широкоэкранник</p>
+    </div>
 
     <button type="button" class="button">
 	    Кнопка
